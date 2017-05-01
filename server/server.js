f51afea7717f90bf4086326051c6ea104dbfda7a
c <rhinoted@gmail.com>
2017-05-01 16:38:22 +0200

    La todo app inizia a prendere forma

--- server/server.js
+++ server/server.js
@@ -1,42 +1,31 @@
-var mongoose = require('mongoose');
+var express=require('express');
+var bodyParser=require('body-parser');
 
-mongoose.Promise=global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp');
+var {mongoose}=require('./db/mongoose.js');
+var {Todo}=require('./models/todo.js');
+var {User}=require('./models/user.js');
 
-var Todo = mongoose.model('Todo', {
-  text:{
-    type:String
-  },
-  completed:{
-    type:Boolean
-  },
-  completedAt:{
-    type:Number
-  }
+
+var app=express();
+
+app.use(bodyParser.json());
+
+app.post('/todos',(req,res)=>{
+
+var todo= new Todo({
+  text:req.body.text
 });
 
-
-var newTodo2 = new Todo({
-  text:'Go Fuck Myself',
-  completed:true,
-  completedAt:3242
-});
-
-
-newTodo2.save().then((doc)=>{
-  console.log('Saved: ',doc);
+todo.save().then((doc)=>{
+  res.send(doc);
 }, (e)=>{
-  console.log('Unable to save',e);
+  res.status(400).send(e);
 });
 
 
-// var newTodo = new Todo({
-//   text:'Cook Dinner'
-// });
-//
-//
-// newTodo.save().then((doc)=>{
-//   console.log('Saved: ',doc);
-// }, (e)=>{
-//   console.log('Unable to save',e);
-// });
+});
+
+
+app.listen(3000,()=>{
+  console.log('Started at port 3000');
+});
