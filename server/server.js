5de6168f38e824504a4f3cadd14a7b16a1bc5ca9
c <rhinoted@gmail.com>
2017-05-02 21:01:36 +0200

    added route to find a single todo note

--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
 var express=require('express');
 var bodyParser=require('body-parser');
 
+
+const {ObjectID} = require('mongodb');
 var {mongoose}=require('./db/mongoose.js');
 var {Todo}=require('./models/todo.js');
 var {User}=require('./models/user.js');
@@ -31,6 +33,26 @@ Todo.find().then((todos)=>{
 },(e)=>{
   res.status(400).send(e);
 });
+
+});
+
+
+
+app.get('/todos/:id',(req,res)=>{
+var id=req.params.id;
+
+if (!ObjectID.isValid(id)) {
+  return res.status(404).send();
+}
+
+Todo.findById(id).then((todo)=>{
+  if (!todo) {
+      return res.status(404).send();
+  }
+  res.status(200).send(todo);
+}).catch((e)=>{
+  res.status(400).send();
+})
 
 });
 
