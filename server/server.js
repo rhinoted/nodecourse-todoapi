cbf73f27bc402a874e4620e714f786a5c539e7f8
c <rhinoted@gmail.com>
2017-05-02 17:17:46 +0200

    add get todo route

--- server/server.js
+++ server/server.js
@@ -21,7 +21,16 @@ todo.save().then((doc)=>{
 }, (e)=>{
   res.status(400).send(e);
 });
+});
 
+
+app.get('/todos',(req,res)=>{
+
+Todo.find().then((todos)=>{
+  res.send(todos);
+},(e)=>{
+  res.status(400).send(e);
+});
 
 });
 
