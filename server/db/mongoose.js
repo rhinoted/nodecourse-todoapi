2431093268d1ff1726b5c19d891c62f337d8d827
c <rhinoted@gmail.com>
2017-05-05 15:23:00 +0200

    test2

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,8 +2,10 @@ var mongoose = require('mongoose');
 
 mongoose.Promise=global.Promise;
 
+// mongoose.connect(process.env.PORT ? 'mongodb://rhinoted:cesare26@ds129651.mlab.com:29651/mydatabase' : 'mongodb://localhost:27017/TodoApp');
+
 // mongoose.connect(process.env.MONGODB_URI);
-mongoose.connect(process.env.PORT ? 'mongodb://rhinoted:cesare26@ds129651.mlab.com:29651/mydatabase' : 'mongodb://localhost:27017/TodoApp');
+mongoose.connect('mongodb://rhinoted:cesare26@ds129651.mlab.com:29651/mydatabase');
 
 
 module.exports={
