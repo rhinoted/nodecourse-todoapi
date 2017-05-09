97d8561df94d180ba146b5e9938c43a7cc588a16
c <rhinoted@gmail.com>
2017-05-09 18:44:50 +0200

    hashing password before saving

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,22 +1,37 @@
+const bcrypt = require('bcryptjs');
+var password ='123abc!';
+bcrypt.genSalt(10,(err,salt)=>{
+  bcrypt.hash(password,salt,(err,hash)=>{
+    console.log(hash);
+  });
+});
+
+
+var hashed='$2a$10$qW2fuqIcDY78ZWPcwpftV.BSY.2vudpsST9GTs98UB/VchcrBiv4S';
+
+bcrypt.compare(password,hashed,(err,res)=>{
+  console.log(res);
+});
+
 // const {SHA256}=require('crypto-js');
 //
 // var message ='porco dio e la madonna';
 // var hash=SHA256(message).toString();
 // console.log(`message ${message}, Hash ${hash}.`);
-
-const jwt= require ('jsonwebtoken');
-
-
-var data= {
-  id:10
-};
-
-
-var token=jwt.sign(data,'secret12312');
-
-console.log(token);
-
-
-var decoded=jwt.verify(token,'secret12312');
-
-console.log(decoded);
+//
+// const jwt= require ('jsonwebtoken');
+//
+//
+// var data= {
+//   id:10
+// };
+//
+//
+// var token=jwt.sign(data,'secret12312');
+//
+// console.log(token);
+//
+//
+// var decoded=jwt.verify(token,'secret12312');
+//
+// console.log(decoded);
